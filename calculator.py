90d588d6a7e81e627abc8eb1a77596821269bbca
wesley-day <wesley.day25@gmail.com>
2023-08-09 18:55:59 -0400

    moved config to its own function

--- calculator.py
+++ calculator.py
@@ -35,20 +35,46 @@ def interpret(expr, graph_mode):
             val = round(val)
         return val
 
+def configure():
+    print("Edit graph domain (domain), number of samples to take for x (samples),",
+          "or rounding threshold (thresh)?\n> ", end="")
+    arg = input()
+    if arg == "domain":
+        print("Min x = ", end="")
+        arg = input()
+        min_x = int(arg) if arg.isnumeric() else None
+        print("Max x = ", end="")
+        arg = input()
+        max_x = int(arg) if arg.isnumeric() else None
+        if not min_x or not max_x or min_x >= max_x:
+            print("Invalid input")
+        else:
+            set_domain(min_x, max_x)
+    elif arg == "samples":
+        print("New number of samples = ", end="")
+        arg = input()
+        if not arg.isnumeric():
+            print("Invalid input")
+        else:
+            global NUM_SAMPLES
+            NUM_SAMPLES = int(arg)
+    elif arg == "thresh":
+        print("New rounding threshold = ", end="")
+        arg = input()
+        if not arg.isnumeric():
+            print("Invalid input")
+        else:
+            global ROUND_THRESH
+            ROUND_THRESH = float(arg)
+
+
 def process_input(line):
     if line == "exit" or line == "quit":
         return False
     if line == "":
         return True
     if line == "configure" or line == "config":
-        print("Min x = ", end="")
-        min_x = int(input())
-        print("Max x = ", end="")
-        max_x = int(input())
-        if min_x >= max_x:
-            print("Min x must be less than max x")
-        else:
-            set_domain(min_x, max_x)
+        configure()
         return True
     try:
         global ans
